75548abbd0c842d51b4a17fbc8bc7acc334eb2fd
scx567888 <scx567888@outlook.com>
2026-07-25 15:00:56 +0800

    v0.10.2

--- src/test/java/dev/scx/web/test/ScxWebTest.java
+++ src/test/java/dev/scx/web/test/ScxWebTest.java
@@ -4,7 +4,7 @@ import dev.scx.http.exception.ForbiddenException;
 import dev.scx.http.routing.Router;
 import dev.scx.http.routing.path_matcher.TemplatePathMatcher;
 import dev.scx.http.x.HttpServer;
-import dev.scx.object.x.mapper.time.TemporalAccessorNodeMapperOptions;
+import dev.scx.object.x.mapper.time.LocalDateTimeNodeMapperOptions;
 import dev.scx.serialize.SerializeConfig;
 import dev.scx.web.ScxWeb;
 import org.testng.annotations.Test;
@@ -61,7 +61,7 @@ public class ScxWebTest {
             .serializeConfig(
                 SerializeConfig.of()
                     .putMapperOptions(
-                        new TemporalAccessorNodeMapperOptions().setFormatter(LocalDateTime.class, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+                        new LocalDateTimeNodeMapperOptions().formatter(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
                     )
             )
             .routes(new HelloWorldController());
